136879d7aaac648ebbc2102c30bc1941765671cd
Brandon King <Brandonkingsemail@gmail.com>
2021-11-05 07:34:11 -0400

    first commit

---
+++ Javascript.js
@@ -0,0 +1,33 @@
+/*
+<p id="demo"></p>
+document.getElementById("demo").innerHTML = "Hello JavaScript"; 
+document.getElementById("demo").style.display = "none"; 
+ <script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script> 
+<script src="myScript2.js"></script>
+ <script src="https://www.w3schools.com/js/myScript.js"></script> 
+console.log()
+window.print()
+let a = 5;
+'' or "" doesnt matter
+// = inline comment
+{
+    let x = 2
+    // Cannot be accessed outside of curly braces
+}
+
+//variables can be hoisted (with var but not with let):
+carName = "Volvo";
+var carName;
+const cars; = constant reference to a value
+function myfunction(){}
+let text= 'It\'s alright.';
+let length = text.length;
+
+    slice(start, end)
+    substring(start, end)
+    substr(start, length)
+text.replace("Microsoft", "W3Schools");
+/MICROSOFT/ig regular expression
+ */
